b2145f7b3364c27ecb3ee2845d75ad11f0fcbd25
kaave <abe@framelunch.jp>
2018-05-24 12:07:45 +0900

    Update production build

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,7 @@
 import './common/initializer';
 
-import mount from './containers/App';
 import '../styles/index.css';
+import mount from './containers/App';
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
